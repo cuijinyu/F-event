6104e5f79f4b2159ea1eb2bf42932c65dbc4d6be
cuijinyu <2510271615@qq.com>
2018-07-10 22:53:25 +0800

    [Add]:add the function once,getListener

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 class F_bus {
     constructor () {
         this.handler = {};
-        this.cache = {};
+        this.handlerOnce = {};
+        this.cache = [];
+        this.nameSpace = {};
     }
     on (event, cb) {
         if (this.handler[event] === undefined) {
             this.handler[event] = [];
         }
         this.handler[event].push(cb);
+    }
+    once (event, cb) {
+        if (this.handlerOnce[event] === undefined) {
+            this.handlerOnce[event] = [];
+        }
+        this.handlerOnce[event].push(cb);
     }
     emit () {
         let key = Array.prototype.shift.apply(arguments);
@@ -16,20 +24,69 @@ class F_bus {
                 this.handler[key][i].apply(this, arguments);
             }
         }
+        if (this.handlerOnce[key] && this.handlerOnce[key].length > 0) {
+            for (let i = 0;i < this.handlerOnce[key].length; i ++) {
+                this.handlerOnce[key][i].apply(this, arguments);
+                this.cache.push({
+                    type:'once',
+                    func:this.handlerOnce[key][i]
+                })
+            }
+            delete this.handlerOnce[key];
+        }
     }
-    remove (event, fun) {
+    remove (event, fun, type) {
         if (!event) {
             return;
         }
         if (!fun) {
             delete this.handler[event];
+            delete this.handlerOnce[event];
         }
-        for (let i = 0;i < this.handler[event].length;i ++) {
-            if (this.handler[event][i] === fun) {
-                delete this.handler[event][i];
-                return;
+        if (type === 'once') {
+            for (let i = 0;i < this.handlerOnce[event].length;i ++) {
+                if (this.handlerOnce[event][i] === fun) {
+                    delete this.handlerOnce[event][i];
+                    return;
+                }
+            }
+        } else if (type === 'normal') {
+            for (let i = 0;i < this.handler[event].length;i ++) {
+                if (this.handler[event][i] === fun) {
+                    delete this.handler[event][i];
+                    return;
+                }
+            }
+        } else {
+            for (let i = 0;i < this.handlerOnce[event].length;i ++) {
+                if (this.handlerOnce[event][i] === fun) {
+                    delete this.handlerOnce[event][i];
+                    return;
+                }
+            }
+            for (let i = 0;i < this.handler[event].length;i ++) {
+                if (this.handler[event][i] === fun) {
+                    delete this.handler[event][i];
+                    return;
+                }
             }
         }
+    }
+    getListener (event) {
+        let result = [];
+        if (this.handlerOnce[event]) {
+            result.push({
+                type:'once',
+                callbackArray:this.handlerOnce[event]
+            })
+        }
+        if (this.handler[event]) {
+            result.push({
+                type:'normal',
+                callbackArray:this.handler[event]
+            })
+        }
+        return result;
     }
 }
 
